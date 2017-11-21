fbf66f64b968bbfa29f64ae196bdbf1d993a298b
made <kike@mazarredos.com>
2017-11-21 16:28:45 +0000

    Primer commit

---
+++ src/pruebas c plus plus.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : pruebas.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "hol nte" << endl; // prints hola gente
+	return 0;
+}
